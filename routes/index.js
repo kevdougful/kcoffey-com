ce12f770cc19df4b1ca74a3202cf9163f184677b
Kevin Coffey <kevdougful@gmail.com>
2015-07-30 00:39:31 -0500

    added contact page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { pagetitle: 'kcoffey.com' });
 });
 
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { pagetitle: 'kcoffey.com' });
+});
+
 module.exports = router;
